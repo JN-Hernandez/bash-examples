a42b5cae57c498ad3cdb26b253489e32fc27cc7f
JN Hernández <jes.nat.hernandez@gmail.com>
2024-10-05 18:27:51 -0600

    Add error handling for disk usage script

--- system-monitoring-script.sh
+++ system-monitoring-script.sh
@@ -1,13 +1,21 @@
-#! /bin/bash
+#!/bin/bash
 # JN Hernández
 # Oct 4, 2024
 # Use: BASH script for monitoring the root directory disk usage on a server
 
 RECIPIENT="monitoring-team@company.com"
 SUBJECT="Root Directory Disk Usage Exceeded 80%"
-
 DISK_USAGE=$(df -h / | awk 'FNR == 2 { print $5 }' | cut -f1 -d"%")
 #DISK_USAGE=$(df -h /dev | awk 'FNR == 2 { print $5 }' | cut -f1 -d"%") #test for 100%
+#DISK_USAGE=$(df -h / | cut -f1 -d"%") #test for nonint
+
+if ! [[ "$DISK_USAGE" =~ ^[0-9]+$ ]]; then
+    echo "$(date): Error: Unable to retrieve disk usage - please investigate \
+as soon as possible." >> disk_usage.log
+    SUBJECT="Unable to retrieve disk usage"
+    tail -1 disk_usage.log | mailx -s "$SUBJECT" "$RECIPIENT"
+    exit 1
+fi
 
 if [ $((DISK_USAGE)) -gt 80 ]; then
     echo "$(date): Warning: Root directory disk usage has exceeded 80% - please investigate \
